62eb1f642b773d7dc5ab481c827f859f58f3d5d5
Diego Gomez <diego.gomez2005@gmail.com>
2017-08-11 13:31:49 -0300

    fixed some memory issues

--- CPU/ProcessorStatus.h
+++ CPU/ProcessorStatus.h
@@ -4,15 +4,14 @@ class ProcessorStatus
 {
 private:
 	uint8_t Status;
-	const uint8_t negative = 0x80;
-	const uint8_t overflow = 0x40;
-	const uint8_t zero = 0x02;
-	const uint8_t carry = 0x01;
-	const uint8_t decimal = 0x08;
-	const uint8_t irqDisable = 0x04;
-	const uint8_t index8bits = 0x10;
-	const uint8_t accumulator8bits = 0x20;
-	const uint8_t flagbreak = 10;
+	const uint8_t carry				= 0x01;
+	const uint8_t zero				= 0x02;
+	const uint8_t irqDisable		= 0x04;
+	const uint8_t decimal			= 0x08;
+	const uint8_t index8bits		= 0x10;
+	const uint8_t accumulator8bits	= 0x20;
+	const uint8_t overflow			= 0x40;
+	const uint8_t negative			= 0x80;
 public:
 	inline void SetStatus(uint8_t status)
 	{
@@ -62,10 +61,5 @@ public:
 	inline bool Is8BitsAccumulator()
 	{
 		return (this->Status & accumulator8bits) == accumulator8bits;
-	}
-
-	inline bool IsBreak()
-	{
-		return (this->Status & flagbreak) == flagbreak;
 	}
 };
--- CPU/Registers.h
+++ CPU/Registers.h
@@ -5,28 +5,28 @@
 struct Registers
 {
 	//The accumulator. This is the math register. It stores one of two operands or the result of most arithmetic and logical operations.
-	DynamicRegister			Accumulator;
+	DynamicRegister				Accumulator;
 
 	//The index registers.These can be used to reference memory, to pass data to memory, or as counters for loops.
-	DynamicRegister			IndexX;
+	DynamicRegister				IndexX;
 	
 	//The index registers. These can be used to reference memory, to pass data to memory, or as counters for loops.
-	DynamicRegister			IndexY;
+	DynamicRegister				IndexY;
 
 	//The stack pointer, points to the next available(unused) location on the stack.
 	uint16_t					StackPointer;
 
 	//Data bank register, holds the default bank for memory transfers
-	uint16_t					DB;
+	uint8_t						DB;
 
 	//Direct page register, used for direct page addressing modes. Holds the memory bank address of the data the CPU is accessing
 	uint16_t					DP;
 
 	//Program Bank, holds the bank address of all instruction fetches.
-	uint16_t					PB;
+	uint8_t						PB;
 
 	//Holds various important flags, results of tests and 65816 processing states
-	ProcessorStatus			Status;
+	ProcessorStatus				Status;
 
 	//Holds the memory address of the current CPU instruction
 	uint16_t					ProgramCounter;
